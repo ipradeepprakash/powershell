eb5e89d15e208c8bca633be72e3957127cecad1d
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2022-12-23 16:36:06 +0530

    Add files via upload

---
+++ FunctionsExamples.ps1
@@ -0,0 +1,16 @@
+﻿#function - to check if employee Age is Major or Minor
+
+function empinfo($name,$age) 
+
+{
+if ($age -gt 18){
+
+Write-Host "Employee $name is ADULT"
+#Write-Host "Emp Name - $name and his age - $age"
+}
+elseif ($age -le 18) {
+
+Write-Host "Employee $name is Minor"
+}
+}
+
