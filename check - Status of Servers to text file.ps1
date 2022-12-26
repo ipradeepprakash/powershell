d2b6ae9f195aa096d2ad7171e8c765c1971b511c
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2022-12-26 11:59:05 +0530

    Add files via upload

---
+++ check - Status of Servers to text file.ps1
@@ -0,0 +1,18 @@
+﻿#-- Powershell script :list of server status sent to text files
+#-- change list of servers, status on: failures, success also location accordingly
+#-----------------------------------------------------------------
+$servers = Get-Content G:\DBAInstalls\testservers.txt
+
+foreach ($server in $servers) {
+
+try{
+Test-Connection -ComputerName $server -Count 1 -ErrorAction Stop
+Add-Content -Path G:\DBAInstalls\successful.txt -value "$server --> CONNECTION is OK "
+}
+
+catch {
+Add-Content -Path G:\DBAInstalls\errors.txt -value "$($_.exception.message) "
+
+}
+
+}
