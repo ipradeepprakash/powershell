669838ff8aa72b4980bc1616e6f5287d1350c3b9
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2023-03-02 12:58:48 +0530

    Add files via upload

---
+++ Check - Drives - FreeSpace Available.ps1
@@ -0,0 +1,13 @@
+﻿cls
+$h = hostname
+Write-Host "Server Name - $h"
+
+Get-Volume | ForEach-Object {
+$DriveL = $_.DriveLetter
+$sizeAlloc = [math]::round(($_.size / 1GB),2)
+$sizeRem = [math]::round(($_.SizeRemaining / 1GB),2)
+
+Write-Host "Drive - $DriveL | Available Size - $sizeAlloc | FreeSpace - $sizeRem "
+
+}
+
