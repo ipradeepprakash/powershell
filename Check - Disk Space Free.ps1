44aa7cb2d19faff76653669854f882a6ac78bd67
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2023-03-03 14:29:14 +0530

    Add files via upload

---
+++ Check - Disk Space Free.ps1
@@ -0,0 +1,4 @@
+﻿cls
+Get-Volume | Select-Object DriveLetter, 
+@{Name='SizeGB';Expression={[math]::Round($_.size / 1GB, 2)}}, 
+@{Name='FreeSpaceGB';Expression={[math]::Round($_.sizeremaining / 1GB, 2)}}
