7f1279a2dbf0da8e7cd342a3b8aa8272471ac125
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2023-03-07 02:41:25 -0600

    Add files via upload
    eventvwr logs - system.
    with "Warning" entrytype

---
+++ Check - Eventvwr - latest 10 logs.ps1
@@ -0,0 +1,6 @@
+﻿cls
+ Get-EventLog -LogName System -After '2023-03-06' -Newest 40 | 
+ where-Object EntryType -eq Warning |
+ ConvertTo-Html | Out-File -FilePath 'c:\sample.html'
+  
+  C:\sample.html
