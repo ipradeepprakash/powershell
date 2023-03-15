8bd57effb8ae31170592930be4ecddaef58fd9d8
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2023-03-15 13:53:48 +0530

    Add files via upload

---
+++ Get-childiteam - Search SPECIFIC Folders-files.ps1
@@ -0,0 +1,8 @@
+﻿cls
+$drives = "C:\test\TestPowershellScripts","C:\temp"
+
+foreach ($a in $drives) {
+Write-Host("***********Folder path - $a*****************")
+Get-ChildItem -Path $a -Recurse -Filter *.ps1| select FullName | fl
+
+}
