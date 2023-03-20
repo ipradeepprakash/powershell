82af6f413141f894e1ac6e15c91f61dbbe662324
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2023-03-20 11:45:01 +0530

    Add files via upload

---
+++ Check - File Extensions in ALL Drives EXCEPT C DRIVE.ps1
@@ -0,0 +1,5 @@
+﻿cls
+$a = Get-PSDrive -PSProvider FileSystem |Where-Object {$_.Name -ne 'c'}| Select-Object -ExpandProperty root
+$a
+Get-ChildItem -Path $a -Recurse | Group-Object Extension | ft -AutoSize
+ 
